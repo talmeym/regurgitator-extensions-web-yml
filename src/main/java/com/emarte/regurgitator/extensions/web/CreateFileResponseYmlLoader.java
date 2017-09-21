e97da270cabdc647676f23fd10bd385876c99229
talmeym <mtalmey@gmail.com>
2017-09-21 11:12:33 +0100

    renamed FileResponse CreateFileResponse

--- src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponseYmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/CreateFileResponseYmlLoader.java
@@ -8,17 +8,17 @@ import static com.emarte.regurgitator.core.CoreConfigConstants.SOURCE;
 import static com.emarte.regurgitator.core.YmlConfigUtil.*;
 import static com.emarte.regurgitator.extensions.web.WebConfigConstants.PATH_PREFIX;
 
-public class FileResponseYmlLoader implements YmlLoader<FileResponse> {
-	private static Log log = Log.getLog(FileResponseYmlLoader.class);
+public class CreateFileResponseYmlLoader implements YmlLoader<CreateFileResponse> {
+	private static Log log = Log.getLog(CreateFileResponseYmlLoader.class);
 
 	@Override
-	public FileResponse load(Yaml yaml, Set<Object> allIds) throws RegurgitatorException {
+	public CreateFileResponse load(Yaml yaml, Set<Object> allIds) throws RegurgitatorException {
 		String id = loadId(yaml, allIds);
 		String source = loadMandatoryStr(yaml, SOURCE);
 		String pathPrefix = loadOptionalStr(yaml, PATH_PREFIX);
 
 		ContextLocation location = source != null ? new ContextLocation(source) : null;
 		log.debug("Loaded file response '" + id + "'");
-		return new FileResponse(id, new ValueSource(location, null), pathPrefix);
+		return new CreateFileResponse(id, new ValueSource(location, null), pathPrefix);
 	}
 }
--- src/main/java/com/emarte/regurgitator/extensions/web/ExtensionsWebYmlPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/ExtensionsWebYmlPackageMap.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ExtensionsWebYmlPackageMap extends AbstractYmlPackageMap {
-	private static final List<String> kinds = Arrays.asList("http-call", "query-param-processor", "create-http-response");
+	private static final List<String> kinds = Arrays.asList("http-call", "query-param-processor", "create-http-response", "create-file-response");
 
 	public ExtensionsWebYmlPackageMap() {
 		addPackageMapping(kinds, "com.emarte.regurgitator.extensions.web");
